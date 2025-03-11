9ac4e4b3e08f288c82d49c2115d177f36f915b9d
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-03-11 21:46:59 +0530

    Update: UpdateContact

--- test/web-test/3_UpdateContact.spec.ts
+++ test/web-test/3_UpdateContact.spec.ts
@@ -5,32 +5,14 @@ test('test', async ({ page }) => {
     await page.goto(baseURL + "contactList");
 
     await page.getByRole('heading', { name: 'Contact List' }).click();
-    // Add Contact
-    await page.getByRole('button', { name: 'Add a New Contact' }).click();
-    await page.getByPlaceholder('First Name').click();
-    await page.getByPlaceholder('First Name').fill('Manikandan');
-    await page.getByPlaceholder('Last Name').click();
-    await page.getByPlaceholder('Last Name').fill('Adaikalam');
-    await page.getByPlaceholder('yyyy-MM-dd').click();
-    await page.getByPlaceholder('yyyy-MM-dd').fill('1996-12-07');
-    await page.getByPlaceholder('example@email.com').click();
-    await page.getByPlaceholder('example@email.com').fill('manizivamsd@gmail.com');
-    await page.getByPlaceholder('8005551234').click();
-    await page.getByPlaceholder('8005551234').fill('8098606357');
-    await page.getByPlaceholder('Address 1').click();
-    await page.getByPlaceholder('Address 1').fill('434, Pudumanai 1st street');
-    await page.getByPlaceholder('Address 2').click();
-    await page.getByPlaceholder('Address 2').fill('Rayavaram');
-    await page.getByPlaceholder('City').click();
-    await page.getByPlaceholder('City').fill('Pudukkottai');
-    await page.getByPlaceholder('State or Province').click();
-    await page.getByPlaceholder('State or Province').fill('Tamilnadu');
-    await page.getByPlaceholder('Postal Code').click();
-    await page.getByPlaceholder('Postal Code').fill('622506');
-    await page.getByPlaceholder('Country').click();
-    await page.getByPlaceholder('Country').fill('India');
+    // Update Contact
+    await page.getByRole('cell', { name: 'Manikandan Adaikalam' }).nth(0).click();
+    await page.getByRole('button', { name: 'Edit Contact' }).click();
+    await page.getByLabel('Country:').click();
+    await page.getByLabel('Country:').fill('SriLanka');
     await page.getByRole('button', { name: 'Submit' }).click();
-    // await page.getByRole('cell', { name: 'Manikandan Adaikalam' }).click();
+    await page.getByRole('button', { name: 'Return to Contact List' }).click();
+
     // Logout and Goto LoginPage
     // await page.getByRole('button', { name: 'Logout' }).click();
     // await page.getByRole('heading', { name: 'Contact List App' }).click();

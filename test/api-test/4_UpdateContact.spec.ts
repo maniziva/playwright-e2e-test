f2e5f52fefe20c45f63d63c32bae30cce514ab12
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-03-15 00:10:09 +0530

    Add: Update and Delete API Scripts

---
+++ test/api-test/4_UpdateContact.spec.ts
@@ -0,0 +1,99 @@
+import { test, expect, request } from '@playwright/test';
+const baseURL = process.env.BASEURL;
+const username = process.env.MAILID as string;
+const password = process.env.PASSWORD;
+
+let token: string;
+let contactId: string;
+
+test.beforeAll('Login', async ({ }) => {
+  // Step 1: Create a new APIRequestContext
+  const apiContext = await request.newContext();
+
+  // Step 2: Send Login Request
+  const loginPayload = {
+    email: username,
+    password: password
+  };
+
+  const loginResponse = await apiContext.post(baseURL + 'users/login', {
+    data: loginPayload
+  });
+
+  // Check if the response is OK
+  expect(loginResponse.ok()).toBeTruthy();
+
+  const loginData = await loginResponse.json();
+  token = loginData.token;
+
+  //console.log('Extracted Token:', token);
+});
+
+test('Get Contact', async ({ }) => {
+  // Step 1: Create a new APIRequestContext
+  const apiContext = await request.newContext();
+  const GetContactResponse = await apiContext.get(baseURL + 'contacts', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+  // Check if the response is successful
+  expect(GetContactResponse.ok()).toBeTruthy();
+
+  const GetContactResponseData = await GetContactResponse.json();
+  //console.log('Get Contact Response:', GetContactResponseData);
+  
+  // Extract the _id value from the first contact
+  const firstContact = GetContactResponseData[0];
+  contactId = firstContact._id;
+  console.log('Contact ID:', contactId);
+
+  // Dispose of the API context
+  await apiContext.dispose();
+});
+
+test('Update Contact', async ({ }) => {
+  // Step 1: Create a new APIRequestContext
+  const apiContext = await request.newContext();
+  const contactPayload = {
+    firstName: "Updated John",
+    lastName: "Doe",
+    birthdate: "1970-01-01",
+    email: "jdoe@fake.com",
+    phone: "8005555555",
+    street1: "1 Main St.",
+    street2: "Apartment A",
+    city: "Anytown",
+    stateProvince: "KS",
+    postalCode: "12345",
+    country: "USA"
+  };
+  const UpdateContactResponse = await apiContext.put(baseURL + 'contacts/' + contactId, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    data: contactPayload
+  });
+  // Check if the response is successful
+  expect(UpdateContactResponse.ok()).toBeTruthy();
+
+  const UpdateContactResponseData = await UpdateContactResponse.json();
+  console.log('Update Contact Response:', UpdateContactResponseData);
+  await apiContext.dispose();
+});
+
+test.afterAll('Logout', async ({ }) => {
+  const apiContext = await request.newContext();
+  // Step 2: Send Logout Request Using the Token
+  const logoutResponse = await apiContext.post(baseURL + 'users/logout', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  // Check if the logout is successful
+  expect(logoutResponse.ok()).toBeTruthy();
+
+  // Dispose of the API context
+  await apiContext.dispose();
+});

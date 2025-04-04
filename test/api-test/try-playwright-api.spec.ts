6bdf79aecf3fdfab218c9064527466dfa13f88ca
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-04-04 17:56:46 +0530

    Update: Debug config

---
+++ test/api-test/try-playwright-api.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect, request } from '@playwright/test';
+const baseURL = process.env.BASEURL;
+const username = process.env.MAILID as string;
+const password = process.env.PASSWORD;
+
+test.only('API response time should be under 2 seconds', async ({  }) => {
+  const thresholdMs = 2000; // 2 seconds
+
+  const startTime = performance.now();
+
+  const apiContext = await request.newContext();
+
+  // Step 2: Send Login Request
+  const loginPayload = {
+    email: username,
+    password: password
+  };
+
+  const response = await apiContext.post(baseURL+'users/login', {
+    data: loginPayload
+  });
+
+  const duration = performance.now()-startTime;
+
+  console.log(`Response time: ${duration} ms`);
+
+  // Optional: Check status
+  expect(response.status()).toBe(200);
+
+  // Assert response time
+  expect(duration).toBeLessThan(thresholdMs);
+
+});

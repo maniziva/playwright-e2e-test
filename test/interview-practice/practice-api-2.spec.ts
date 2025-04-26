827f89ed67405bcfa9cff291fcde15df170f790e
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-04-26 09:21:23 +0530

    update: json and RegEx validation

--- test/interview-practice/practice-api-2.spec.ts
+++ test/interview-practice/practice-api-2.spec.ts
@@ -7,9 +7,22 @@ test.describe.parallel('API Practice', () => {
     const response = await apiRequest.get('users/me');
     expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(200);
+  });
 
+  test('Get contact - Response Assertion - Json Validation', async ({ apiRequest }) => {
+    const response = await apiRequest.get('users/me');
     const data = await response.json();
-    console.log(data);
+    //console.log(data);
     expect(data.email).toContain('master@gmail.com');
+  });
+
+  test('Get contact - Response Assertion - RegEx Validation', async ({ apiRequest }) => {
+    const response = await apiRequest.get('users/me');
+    const data = await response.json();
+    //console.log(data);
+    const text = JSON.stringify(data);
+    //console.log(text);
+    let matches = text.match(/"email":"(.*?)@gmail.com/i);
+    console.log(matches[1]); 
   });
 });

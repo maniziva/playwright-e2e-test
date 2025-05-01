2329153fefc9d57cbb6c85bdfad3ba8c19ecd5b1
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-05-01 11:41:02 +0530

    extract json from api

--- test/interview-practice/practice-api.spec.ts
+++ test/interview-practice/practice-api.spec.ts
@@ -34,4 +34,32 @@ test.describe.parallel("API Practice", async () => {
     console.log(getResponseData);
     expect(getResponseData.email).toContain("master@gmail.com");
   });
+
+  test.only("Get My name", async ({ request }) => {
+    const getResponse = await request.get("contacts", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const jsonData = await getResponse.json();
+    // const name = jsonData
+    //   .filter(emp => emp._id === '680fac5591094c00157ae9a9')
+    //   .map(emp => emp.firstName);
+    
+    // console.log("Matched name:", name[0]); // ['John'] or []
+
+
+    const reg: RegExp = /"_id":"(.*?)",/i;
+    const phone = jsonData.filter(emp => emp.phone?.includes("8098"));
+    console.log(phone);
+
+    //const jsonString = JSON.stringify(phone);
+    //console.log(jsonString);
+
+
+    const id: string[] = phone.map(emp => emp._id);
+    console.log(`Extracted ID is: ${id.length}`);
+
+  });
+  
 });

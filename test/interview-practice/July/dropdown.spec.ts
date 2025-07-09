5e752ff1f521e1d0c095dd21e0aa539807f2ea8a
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-07-09 13:21:05 +0530

    update: multi dropdown

--- test/interview-practice/July/dropdown.spec.ts
+++ test/interview-practice/July/dropdown.spec.ts
@@ -1,17 +1,38 @@
 import {expect, Locator, test} from '@playwright/test';
+import { Console } from 'console';
+import exp from 'constants';
 
 test.skip('dropdown', async({page})=>{
     await page.goto('https://testautomationpractice.blogspot.com/');
     await page.waitForLoadState('networkidle');
     await expect(page).toHaveTitle('Automation Testing Practice');
     
-    const multi = page.locator('#colors');
+    const multi = page.locator('#colors');c
+
+    const values = await multi.evaluate((select) => {
+      return [...(select as HTMLSelectElement).options].map(option => option.value);
+    });
+    console.table(values);
+
     await multi.selectOption([{ value: 'red' }, { value: 'blue' }]);
-    //await multi.screenshot({ path: 'src/download/multi.png' })
+    await multi.scrollIntoViewIfNeeded();
     
     const selectedValues = await multi.evaluate((select) => {
       return [...(select as HTMLSelectElement).selectedOptions].map(option => option.value);
     });
-    
     console.log(selectedValues);
+    expect(selectedValues).toEqual(['red', 'blue']);
+})
+
+test('single dropdown', async({page}) =>{
+  await page.goto('https://testautomationpractice.blogspot.com/');
+  await page.waitForLoadState('networkidle');
+  await expect(page).toHaveTitle('Automation Testing Practice');
+
+  const drop = await page.getByRole('combobox', {name: 'Country'})
+  await drop.selectOption({value: 'canada'});
+  const selected = await drop.inputValue();
+  //console.log(selected);
+  //await expect(selected).toBe('canada');
+  await expect(drop).toHaveValue('canada');
 })

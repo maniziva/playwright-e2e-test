389c5a537cef5ec7d0739bb45fd1f499a2d31cca
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-12-22 17:34:07 +0530

    Add IST timestamp formatting to logger and new test case
    Updated logger to format timestamps in IST using Intl.DateTimeFormat. Improved table data typing and formatting in test, and added a new test for infinite scroll data extraction.

--- test/interview-practice/practice2/dec22-practice.spec.ts
+++ test/interview-practice/practice2/dec22-practice.spec.ts
@@ -6,7 +6,7 @@ test.describe("Dec-22 Test Suite", async () => {
   test.beforeEach("Before", async ({ page }) => {
     await page.goto(baseURL);
   });
-  test("Get Table values with pagination", async ({ page }) => {
+  test("1. Get Table values with pagination", async ({ page }) => {
     const pagination = page.locator('ul[id="pagination"] li');
     const pageCount = await pagination.count();
     console.log(pageCount);
@@ -19,7 +19,7 @@ test.describe("Dec-22 Test Suite", async () => {
       const rowCount = await tableRow.count();
       console.log(`Number of rows ${rowCount}`);
 
-      const tableData:{ID: string, Name: string, Price: string}[] = [];
+      const tableData: { ID: string; Name: string; Price: string }[] = [];
       for (let j = 0; j < rowCount; j++) {
         const currentRow = await tableRow.nth(j);
         const id = await currentRow.locator("td").nth(0).innerText();
@@ -28,14 +28,33 @@ test.describe("Dec-22 Test Suite", async () => {
         //const select = await currentRow.locator('td input').nth(4).isChecked();
 
         tableData.push({
-            ID: id,
-            Name: name,
-            Price: price,
-        })
+          ID: id,
+          Name: name,
+          Price: price,
+        });
       }
       console.table(tableData);
     }
+  });
+  test("2. Scroll and get data", async ({ page }) => {
+    await page.goto("https://infinite-scroll.com/demo/full-page/");
+    const articles = await page.locator('article[class="article"]');
+    const aricleCount = await articles.count();
+    console.log(aricleCount);
 
+    let maxScrolls = 100;
+    let previousHeight = 0;
 
+    for (let i = 0; i < maxScrolls; i++) {
+      await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+      await page.waitForTimeout(1500); // allow time for loading
+
+      const currentHeight = await page.evaluate(() => document.body.scrollHeight);
+      if (currentHeight === previousHeight) {
+        console.log('Height reached');
+        break;
+      }
+      previousHeight = currentHeight;
+    }
   });
 });
